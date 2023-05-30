4945cbb663e514691d297bd3d783b35a2f22f408
jhon <jjmorgar.co@gmail.com>
2023-05-30 15:23:23 -0500

    feat(ejercicios): :sparkles: punto 6

--- index.php
+++ index.php
@@ -12,4 +12,29 @@
  */
  echo '<h1>hola mundo cruel</h1>';
 
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   punto 6                                  */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * funciones de salidar
+ * mostrar información al usuario cuando finaliza la ejecución de un proceso determinado o cuando finaliza el
+ * proceso una petición realizada por el usuario final en la salida estandar.
+ */
+
+
+//mostrar una varias string
+echo 'texto a imprimir';
+
+// imprimir con formato
+$strSaludo = "mundi"; 
+printf("hola mundo %s ", $strSaludo);
+
+// devuelve el texto formateado NO LO IMPRIME
+$mensaje = sprintf("hola %s", $strSaludo);
+echo $mensaje;
+
 ?>
