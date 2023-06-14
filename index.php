ccc2f0c5a1202dce54440d61cf8ef1d8ba50df3f
JJmoraless <JJmoraless>
2023-06-13 21:13:41 -0500

    feat: :sparkles: punto 9

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ echo $mensaje;
 /* -------------------------------------------------------------------------- */
 /*                                   punto 8                                  */
 /* -------------------------------------------------------------------------- */
-
+echo '<hr>';
 
 
 /**
@@ -92,6 +92,108 @@ $float = 200.5;
 $nombre ="juan";
 $array = [];
 
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   punto 9                                  */
+/* -------------------------------------------------------------------------- */
+/**
+ * numeros y operadores
+ */
+
+
+$num1 = 20;
+$num2 = 30;
+$num3 = 30;
+$num3 = "30";
+var_dump($num1 > $num2);
+echo "<br/>";
+
+var_dump($num1 <=> $num2);
+echo "<br/>";
+
+
+// -1 si izquierda es menor
+// 0 si es igual 
+// 1 si izquirda es mayor
+
+var_dump($num2 <=> $num1);
+echo "<br/>";
+
+
+/**
+ * metodos de cadenas
+ */
+
+$nombre_cliente = " AAA  campers campuslands";
+
+// extension de un str
+echo strlen($nombre_cliente);
+
+echo "<br/>";
+// eliminar expaciones en blanco alrededor
+$texto = trim($nombre_cliente);
+echo $texto;
+echo "<br/>";
+
+// convertir a mayuscula
+echo strtoupper($texto);
+echo "<br/>";
+
+// convertir a minuscula
+echo strtolower($texto);
+echo "<br/>";
+
+// touper y tolower
+$email1 = "correo@correo.com";
+$email2 = "Correo@correo.com";
+var_dump( strtolower($email1) ===  strtolower($email2));
+echo "<br/>";
+
+
+// replazar (buscado, remplazo, texto) 
+echo str_replace("AAA","J", $nombre_cliente);
+echo "<br/>";
+
+
+echo "<h3> Arreglos, Arreglos asociativos y funciones para arreglos </h3>";
+$carrito = array("tablet","tv", "computer");
+
+
+// buscar elementos en un array
+// tambien busca arrays en un arrays de arrays
+// devuelve booleano
+
+$carrito[] = "new product";
+var_dump($carrito) ;
+$a = array(array('p', 'h'), array('p', 'r'), 'o');
+
+if (in_array(array('p', 'h'), $a)) {
+    echo "Se encontró 'ph'\n";
+}
+
+if (in_array(array('p'), $a)) {
+    echo "Se encontró 'fi'\n";
+}
+
+if (in_array('o', $a)) {
+    echo "Se encontró 'o'\n";
+}
+
+/**
+ * El resultado del ejemplo sería:
+ *  Se encontró 'ph'
+ *  Se encontró 'o'
+ */
+
+
+// ordenar elementos de un arreglo
+// si lo muta
+$numeros = array(312,31,3,12,3);
+sort($numeros); // de menor a mayor
+rsort($numeros); // de mayor a menor
+
+var_dump($numeros);
 
 
 ?>
